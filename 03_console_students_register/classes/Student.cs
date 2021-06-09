39d47c95a58df109e1a7ec043f1d67107477909a
5369pc_lenon <5369pc_lenon@callink.com.br>
2021-06-09 20:41:59 -0300

    First commit

--- 03_console_students_register/classes/Student.cs
+++
@@ -1,6 +0,0 @@
-namespace _03_review {
-    public struct Student {
-        public string Name;        
-        public decimal Grade;
-    } //End struct Student;
-}
---
+++ devFitness_Console/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace devFitness_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Digite sua altura");
+            string heigth = Console.ReadLine();
+
+            Console.WriteLine("Digite seu peso");
+            string weight = Console.ReadLine();
+            
+            showOptions();
+
+            string option = Console.ReadLine();
+
+            if(option == "1") {
+                Console.WriteLine($"Nome: {name}, Altura: {heigth}, Peso: {weight}");
+            }
+
+            else if (option == "2") {
+                //Register(new List<Meal>);
+            }
+
+            else if (option == "3") {
+                //ShowMeals();
+            }
+
+            else {
+
+            }
+        } //Main
+
+        public static void showOptions() {
+            Console.WriteLine("--- Bem-vindo (a) ao devFitness ---");
+            Console.WriteLine("1 - Exibir detalhes de usuário");
+            Console.WriteLine("2 - Cadastrar nova refeição");
+            Console.WriteLine("3 - Listar todas as refeições do dia");
+            Console.WriteLine("4 - Encerrar aplicação");
+        } //showOptions
+
+        public static void Register() {
+
+        }
+
+        public static void ShowMeals() {
+            
+        }
+    }
+}
